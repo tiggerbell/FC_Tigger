424abdfc14b55df6b6ef55a0397b0e95c4b9e941
JongChan-Kim98 <whdcks9920@gmail.com>
2023-10-12 14:20:54 +0900

    골, 어시 이미지 적용

--- src/Page/Main.js
+++ src/Page/Main.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../style/Main.scss";
+import assist from "../assets/images/assist.svg";
+import goal from "../assets/images/goal.svg";
 
 const Main = () => {
   return (
@@ -13,32 +15,102 @@ const Main = () => {
           <div className="first-dan__center">
             <div className="center__score-box">
               {/* 스코어 수정 */}
-              <span className="score-box__home-score">3</span>
-              <span className="score-box__away-score">3</span>
+              <span className="score-box__home-score">1</span>
+              <span className="score-box__away-score">5</span>
             </div>
             {/* 날짜 구장 수정 */}
-            <h1 className="center__date">10.07 09:30</h1>
-            <h2 className="center__place">난지천공원 풋살장</h2>
+            <h1 className="center__date">10.11 20:00</h1>
+            <h2 className="center__place">과천 관문체육공원 풋살장 B</h2>
           </div>
           <div className="first-dan__right">
             {/* away 이름 수정 */}
-            <span className="left__team-name">FC 언약</span>
+            <span className="left__team-name">풋살엔조이</span>
           </div>
         </div>
         {/* 2열 참가 인원 정보 */}
         <div className="container__secon-dan">
           <div className="secon-dan__home">
-            <span className="home__member">고동혁</span>
-            {/* <span className="home__member">김민규</span> */}
-            <span className="home__member">김원형</span>
-            <span className="home__member">김종찬</span>
-            {/* <span className="home__member">나상우</span> */}
-            <span className="home__member">박남주</span>
-            {/* <span className="home__member">박한규</span> */}
-            <span className="home__member">안주영</span>
-            <span className="home__member">이병찬</span>
-            <span className="home__member">임영훈</span>
-            {/* <span className="home__member blur">No Show 안주영</span> */}
+            <div className="home__player-box">
+              <span className="player-box__text">1</span>
+              <img
+                className="player-box__assist"
+                src={assist}
+                alt="icon"
+                width={24}
+              />
+              <span className="player-box__member">고동혁</span>
+            </div>
+            <div className="home__player-box">
+              <span className="player-box__text">1</span>
+              <img
+                className="player-box__goal"
+                src={goal}
+                alt="icon"
+                width={24}
+              />
+              <span className="player-box__text">1</span>
+              <img
+                className="player-box__assist"
+                src={assist}
+                alt="icon"
+                width={24}
+              />
+              <span className="player-box__member">김수겸</span>
+            </div>
+            {/* <span className="player-box__member">김민규</span> */}
+
+            {/* <span className="player-box__member">김원형</span> */}
+            <div className="home__player-box">
+              <span className="player-box__member">김종찬</span>
+            </div>
+            <div className="home__player-box">
+              <span className="player-box__text">1</span>
+              <img
+                className="player-box__assist"
+                src={assist}
+                alt="icon"
+                width={24}
+              />
+              <span className="player-box__member">나상우</span>
+            </div>
+            <div className="home__player-box">
+              <span className="player-box__text">1</span>
+              <img
+                className="player-box__assist"
+                src={assist}
+                alt="icon"
+                width={24}
+              />
+              <span className="player-box__member">박남주</span>
+            </div>
+            {/* <span className="player-box__member">박한규</span> */}
+
+            <div className="home__player-box">
+              <span className="player-box__member">안주영</span>
+            </div>
+            {/* <span className="player-box__member">이병찬</span> */}
+            <div className="home__player-box">
+              <span className="player-box__text">1</span>
+              <img
+                className="player-box__goal"
+                src={goal}
+                alt="icon"
+                width={24}
+              />
+              <span className="player-box__member">이준민</span>
+            </div>
+            <div className="home__player-box">
+              <span className="player-box__text">2</span>
+              <img
+                className="player-box__goal"
+                src={goal}
+                alt="icon"
+                width={24}
+              />
+              <span className="player-box__member">임영훈</span>
+            </div>
+
+            {/* <span className="player-box__member blur">No Show 안주영</span> */}
           </div>
           <div className="secon-dan__away"></div>
         </div>
