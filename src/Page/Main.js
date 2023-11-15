7ea7fbaea475a74de06eea3052c97aafe5d1dfa3
JongChan-Kim98 <whdcks9920@gmail.com>
2023-11-16 00:04:43 +0900

    배열 리스트로 정리하기

--- src/Page/Main.js
+++ src/Page/Main.js
@@ -4,21 +4,22 @@ import assist from "../assets/images/assist.svg";
 import goal from "../assets/images/goal.svg";
 
 const Main = () => {
-  const [homeTotal, setHomeTotal] = useState(0);
-  const calculateTotalGoalHome = () => {
-    const player1 = 2;
-    const player2 = 3;
-    const player3 = 5;
-    const player4 = 5;
-    const player5 = 5;
-    const player6 = 5;
+  const [homeTotalGoal, setHomeTotalGoal] = useState(0);
+  const [homeTotalAssist, setHomeTotalAssist] = useState(0);
 
-    // 입력한 값들을 합산
-    const sum = player1 + player2 + player3 + player4 + player5 + player6;
-
-    // 합산 결과를 상태 변수에 설정
-    setHomeTotal(sum);
-  };
+  const homePlayer1 = [0, 1]; // 고동혁
+  const homePlayer2 = [0, 1]; // 김수겸
+  const homePlayer3 = [0, 1]; // 김민규
+  const homePlayer4 = [0, 1]; // 김원형
+  const homePlayer5 = [0, 1]; // 김종찬
+  const homePlayer6 = [0, 1]; // 민경준
+  const homePlayer7 = [0, 1]; // 나상우
+  const homePlayer8 = [0, 1]; // 박남주
+  const homePlayer9 = [0, 1]; // 박한규
+  const homePlayer10 = [0, 1]; // 안주영
+  const homePlayer11 = [0, 1]; // 이병찬
+  const homePlayer12 = [0, 1]; // 이준민
+  const homePlayer13 = [0, 1]; // 임영훈
 
   return (
     <div className="main">
@@ -47,13 +48,13 @@ const Main = () => {
         <div className="container__secon-dan">
           <div className="secon-dan__home">
             <div className="home__player-box">
-              {/* <span className="player-box__text">5</span>
+              <span className="player-box__text">5</span>
               <img
                 className="player-box__goal"
                 src={goal}
                 alt="icon"
                 width={24}
-              /> */}
+              />
               <span className="player-box__text">1</span>
               <img
                 className="player-box__assist"
@@ -63,7 +64,7 @@ const Main = () => {
               />
               <span className="player-box__member">고동혁</span>
             </div>
-            {/* <div className="home__player-box">
+            <div className="home__player-box">
               <span className="player-box__text">1</span>
               <img
                 className="player-box__goal"
@@ -79,8 +80,8 @@ const Main = () => {
                 width={24}
               />
               <span className="player-box__member">김수겸</span>
-            </div> */}
-            <div className="home__player-box">
+            </div>
+            {/* <div className="home__player-box">
               <span className="player-box__text">2</span>
               <img
                 className="player-box__goal"
@@ -96,7 +97,7 @@ const Main = () => {
                 width={24}
               />
               <span className="player-box__member">김민규</span>
-            </div>
+            </div> */}
             {/* <div className="home__player-box">
               <span className="player-box__text">2</span>
               <img
@@ -114,7 +115,7 @@ const Main = () => {
               />
               <span className="player-box__member">김원형</span>
             </div> */}
-            <div className="home__player-box">
+            {/* <div className="home__player-box">
               <span className="player-box__text">1</span>
               <img
                 className="player-box__goal"
@@ -122,15 +123,15 @@ const Main = () => {
                 alt="icon"
                 width={24}
               />
-              {/* <span className="player-box__text">2</span>
+              <span className="player-box__text">2</span>
               <img
                 className="player-box__assist"
                 src={assist}
                 alt="icon"
                 width={24}
-              /> */}
+              />
               <span className="player-box__member">김종찬</span>
-            </div>
+            </div> */}
             {/* <div className="home__player-box">
               <span className="player-box__text">5</span>
               <img
@@ -148,7 +149,7 @@ const Main = () => {
               />
               <span className="player-box__member">민경준</span>
             </div> */}
-            <div className="home__player-box">
+            {/* <div className="home__player-box">
               <span className="player-box__text">1</span>
               <img
                 className="player-box__assist"
@@ -191,9 +192,9 @@ const Main = () => {
                 width={24}
               />
               <span className="player-box__member">박한규</span>
-            </div>
+            </div> */}
 
-            <div className="home__player-box">
+            {/* <div className="home__player-box">
               <span className="player-box__text">2</span>
               <img
                 className="player-box__goal"
@@ -201,15 +202,15 @@ const Main = () => {
                 alt="icon"
                 width={24}
               />
-              {/* <span className="player-box__text">3</span>
+              <span className="player-box__text">3</span>
               <img
                 className="player-box__assist"
                 src={assist}
                 alt="icon"
                 width={24}
-              /> */}
+              />
               <span className="player-box__member">안주영</span>
-            </div>
+            </div> */}
             {/* <div className="home__player-box">
               <span className="player-box__text">6</span>
               <img
@@ -263,11 +264,9 @@ const Main = () => {
         {/* 3열 총 득점, 어시스트 정보 */}
         <div className="container__third-dan">
           <div className="third-dan__home">
-            <span className="score-text" onChange={calculateTotalGoalHome}>
-              {homeTotal}
-            </span>
+            <span className="score-text">{homeTotalGoal}</span>
             <img className="team-goal" src={goal} alt="icon" width={24} />
-            <span className="score-text">6</span>
+            <span className="score-text">{homeTotalAssist}</span>
             <img className="team-assist" src={assist} alt="icon" width={24} />
           </div>
           <div className="third-dan__away"></div>
